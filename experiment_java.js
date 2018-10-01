bd51f59d461a95e8f77b4e25636e93ae97aca348
degeeter <danielledegeeter@aol.com>
2018-10-01 17:44:47 -0400

    Add files via upload

---
+++ experiment_java.js
@@ -0,0 +1,18 @@
+
+
+
+var $form = $('form#Test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwaKbnIzLxV-T4Jv_QDgAfpKilsXvvD4PCamXXUg5egMcZjR7PQ/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject(),
+    success: function(){
+      console.log("yay");
+    }
+  )};
+});
